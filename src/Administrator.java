08c2da2b124606a911b5edac44db4a71de0ecd93
maiacamarero <maia.camarero@ing.austral.edu.ar>
2019-11-10 18:04:27 -0300

    11

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,5 +1,9 @@
 public class Administrator extends User {
 
+    public Administrator(String username, String phoneNumber) {
+        super(username, phoneNumber);
+    }
+
     @Override
     public void startTrip() {
 
--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,9 @@
 public class Client extends User {
 
+    public Client(String username, String phoneNumber) {
+        super(username, phoneNumber);
+    }
+
     @Override
     public void startTrip() {
 
